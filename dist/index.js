d8a73a9927d2754478e633ef43d62c79f0cdc9d8
Yosmel Chiang <yosmelchiang@gmail.com>
2026-01-21 21:33:26 +0100

    feat: add initial project files

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello ${name}`;
+}
+console.log(greet("world"));
+//# sourceMappingURL=index.js.map
